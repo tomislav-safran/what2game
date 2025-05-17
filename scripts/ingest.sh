1d14a6175b2b85385c0c7dc454181a411e0380c1
Tomislav Šafran <166232203+tomislav-safran@users.noreply.github.com>
2025-05-17 16:40:14 +0200

    Update ingest.sh

--- scripts/ingest.sh
+++ scripts/ingest.sh
@@ -1,4 +1,7 @@
 docker compose --profile ingest run --rm ingest
 
+# or
+docker compose --profile ingest up -d ingest
+
 # Remove the ingest image after it's done
 docker rmi what2game-ingest || echo "Image already removed or not found."
