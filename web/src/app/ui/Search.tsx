81bfd16da11fdb8d3d591a2168d4444ebef26b50
Tomislav <tsafran222@gmail.com>
2025-05-21 17:09:24 +0200

    fix casing

--- web/src/app/ui/Search.tsx
+++ web/src/app/ui/Search.tsx
@@ -4,7 +4,7 @@ import {MagnifyingGlassIcon} from '@heroicons/react/24/outline';
 import {useSearchParams, usePathname, useRouter} from 'next/navigation';
 import {useDebouncedCallback} from 'use-debounce';
 
-export default function Search_rename({placeholder}: { placeholder: string }) {
+export default function Search({placeholder}: { placeholder: string }) {
     const searchParams = useSearchParams();
     const pathname = usePathname();
     const {replace} = useRouter();
